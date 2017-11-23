1a7ac18503fd4823d56c9a3a37a0057216ddf796
Marcus <marcus_amaral@msn.com>
2017-11-23 12:38:41 -0200

    Adicionando CSS e melhoria na task de server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
-var express = require("express");
-var app = express();
-var port = 3000;
-var bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
+const express = require("express");
+const app = express();
+const port = 3000;
+const opn = require('opn');
 
 // Define routes
 app.use(express.static("public"));
 
 // Start the server
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`http://localhost:${port}`);
+  opn(`http://localhost:${port}`);
 });
